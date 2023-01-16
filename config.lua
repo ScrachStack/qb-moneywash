136d2c6f21a74c95efc9528ef04e305534d0c9a1
KeziCodesSometimes <122732007+KeziCodesSometimes@users.noreply.github.com>
2023-01-15 19:22:39 -0600

    Update config.lua

--- config.lua
+++ config.lua
@@ -1,8 +1,11 @@
 -- JOIN FOR SUPPORT https://discord.gg/qRQ5f5rdmN
-Config = {}
+Config = {
+    Tax = 25,
+    location = {x = 1136.79, y = -989.22, z = 46.11}
+}
 
+Config.Misc = {
+    UseprogressCircle = false,
+    Progressduration = 2000
+}
 
-Config.Tax = 25
-Config.location = {x = 1136.79, y = -989.22, z = 46.11}
-Config.UseprogressCircle = false
-Config.Progressduration = 2000 -- in millisecond (only works if UseprogressCircle on :)
