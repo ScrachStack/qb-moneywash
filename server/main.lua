802e13eb1078077ac0d4c46c7fd9143c0e831d72
KeziCodesSometimes <122732007+KeziCodesSometimes@users.noreply.github.com>
2023-05-20 15:50:04 -0500

    Update main.lua

--- server/main.lua
+++ server/main.lua
@@ -1,34 +1,42 @@
 local QBCore = exports['qb-core']:GetCoreObject()
 
+RegisterNetEvent('kezi:moneywash', function(amount)
+    local src = source
+    local Player = QBCore.Functions.GetPlayer(src)
 
-RegisterNetEvent('kezi:moneywash', function (amount) -- Wooshie#1337 Fixing 
-   local src = source
-   local Player = QBCore.Functions.GetPlayer(src)
+    if tonumber(amount) < 0 then return end
 
-   if tonumber(amount) < 0 then return end
+    local currentLocation = nil
+    local location = GetEntityCoords(PlayerPedId())
+    for _, loc in ipairs(Config.Locations) do
+        local dist = #(location - vector3(loc.x, loc.y, loc.z))
+        if dist < 5.0 then
+            currentLocation = loc
+            break
+        end
+    end
 
+    if not currentLocation then
+        print("Location not found")
+        return
+    end
 
-   
-   bidentax = (amount * Config.Tax)  / 100
-   newAmount = amount - bidentax
+    local tax = tonumber(currentLocation.tax)
+    local bidentax = (amount * tax) / 100
+    local newAmount = amount - bidentax
 
-   if Player.Functions.RemoveMoney("blackmoney", math.ceil(amount)) then
-       Wait(1000)
-       Player.Functions.AddMoney('cash', newAmount)
-   end
+    if Player.Functions.RemoveMoney("blackmoney", math.ceil(amount)) then
+        Wait(1000)
+        Player.Functions.AddMoney('cash', newAmount)
+    end
 end)
 
 function startup()
-print([[
-[KZ] Kezi-Moneywash has started. | Verison 1.0.3
-]])
+    print("[KZ] Kezi-Moneywash has started. | Version 1.0.3")
 end
 
-
-
 AddEventHandler('onResourceStart', function(resource)
-   if resource == GetCurrentResourceName() then
-    startup()
-   end
+    if resource == GetCurrentResourceName() then
+        startup()
+    end
 end)
-
