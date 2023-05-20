3d99a32380a5c774cf3b76b53b6cf23b1420aa18
KeziCodesSometimes <122732007+KeziCodesSometimes@users.noreply.github.com>
2023-05-20 13:07:55 -0500

    Update config.lua

--- config.lua
+++ config.lua
@@ -1,11 +1,13 @@
 -- JOIN FOR SUPPORT https://discord.gg/qRQ5f5rdmN
 Config = {
-    Tax = 25,
-    location = {x = 1136.79, y = -989.22, z = 46.11}
+    Locations = {
+        { name = "Location 1", x = 1136.79, y = -989.22, z = 46.11, tax = 10 },
+        { name = "Location 2", x = 1234.56, y = -789.01, z = 32.10, tax = 15 },
+        { name = "Location 3", x = 987.65, y = -543.21, z = 12.34, tax = 20 },
+    },
 }
 
 Config.Misc = {
     UseprogressCircle = false,
-    Progressduration = 2000
+    Progressduration = 2000,
 }
-
