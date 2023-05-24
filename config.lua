e0cae339fb27d3fcd4f123534c79fde2944df2bb
KeziCodesSometimes <122732007+KeziCodesSometimes@users.noreply.github.com>
2023-05-24 01:49:19 -0500

    Update config.lua

--- config.lua
+++ config.lua
@@ -1,7 +1,7 @@
 -- JOIN FOR SUPPORT https://discord.gg/qRQ5f5rdmN
 Config = {
     Locations = {
-        { name = "Location 1", x = 1136.79, y = -989.22, z = 46.11, tax = 10 },
+        { name = "Moneywash", x = 1136.79, y = -989.22, z = 46.11, tax = 10 }, -- this is the location from the one on the fourms post!
         { name = "Location 2", x = 1234.56, y = -789.01, z = 32.10, tax = 15 },
         { name = "Location 3", x = 987.65, y = -543.21, z = 12.34, tax = 20 },
     },
