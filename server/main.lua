baf823cbed2947c15a804a29b638790913d961d7
Zaps6000 <122732007+Zaps6000@users.noreply.github.com>
2023-09-18 19:14:06 -0700

    Update main.lua

--- server/main.lua
+++ server/main.lua
@@ -32,11 +32,52 @@ RegisterNetEvent('kezi:moneywash', function(amount, playerCoords)
 end)
 
 function startup()
-    print("[KZ] Kezi-Moneywash has started. | Version 1.0.4")
+    print("[Zaps] QB-Moneywash has started. | Version 6.2")
+end
+function zapsupdatee()
+local githubRawUrl = "https://raw.githubusercontent.com/Zaps6000/base/main/api.json"
+local resourceName = "moneywash" 
+
+PerformHttpRequest(githubRawUrl, function(statusCode, responseText, headers)
+    if statusCode == 200 then
+        local responseData = json.decode(responseText)
+
+        if responseData[resourceName] then
+            local remoteVersion = responseData[resourceName].version
+            local description = responseData[resourceName].description
+            local changelog = responseData[resourceName].changelog
+
+            local manifestVersion = GetResourceMetadata(GetCurrentResourceName(), "version", 0)
+
+            print("Resource: " .. resourceName)
+            print("Manifest Version: " .. manifestVersion)
+            print("Remote Version: " .. remoteVersion)
+            print("Description: " .. description)
+
+            if manifestVersion ~= remoteVersion then
+                print("Status: Out of Date (New Version: " .. remoteVersion .. ")")
+                print("Changelog:")
+                for _, change in ipairs(changelog) do
+                    print("- " .. change)
+                end
+                print("Link to Updates: https://zaps6000.tebex.io/ https://discord.gg/cfxdev")
+            else
+                print("Status: Up to Date")
+            end
+        else
+            print("Resource name not found in the response.")
+        end
+    else
+        print("HTTP request failed with status code: " .. statusCode)
+    end
+end, "GET", nil, json.encode({}), {})
 end
 
 AddEventHandler('onResourceStart', function(resource)
     if resource == GetCurrentResourceName() then
         startup()
+        zapsupdatee()
     end
 end)
+
+
